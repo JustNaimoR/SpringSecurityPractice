cc6a0c8670484302013789787aa6368b3add8d9b
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2024-03-18 17:38:23 +0300

    Initial commit

---
+++ src/test/java/romario/education/SpringSecurityPractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package romario.education.SpringSecurityPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
